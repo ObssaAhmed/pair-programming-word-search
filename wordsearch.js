ecb796f3407a0f5cd0dc4c642ea5d441e225afd5
Obssa <obssa.ahmed@torontomu.ca>
2023-02-24 02:52:46 -0500

    Adding wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,13 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false; // edge case: empty array
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+    for (const line of [...horizontalJoin, ...verticalJoin]) {
+        if (line.includes(word)) return true;
+    }
+    return false;
+};
+
+module.exports = wordSearch;
